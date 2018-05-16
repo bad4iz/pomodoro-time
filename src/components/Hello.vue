91718c344c5f40d81997b4bc2e30e566ed02ab1c
bad4iz <info@bad4iz.ru>
2018-05-16 21:52:36 +0400

    сама крутилка

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,22 +2,57 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-       <div class="circle">
-         <div class="str"></div>
-       </div>
+        <div class="circle">
+          <div class="str"></div>
+        </div>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
+<script>
+  /* eslint-disable no-shadow */
 
+  export default {
+    data() {
+      return {
+        title: 'Vuetify.js',
+      };
+    },
+    created() {
+
+    },
+    mounted() {
+      const circle = document.querySelector('.circle');
+      circle.ondragstart = () => false;
+      circle.onmousedown = (event) => {
+        circle.style.position = 'absolute';
+        console.log(event);
+        function moveAt(event) {
+// eslint-disable-next-line no-mixed-operators
+          console.log(`${event.pageX - circle.offsetWidth / 2}px`);
+          circle.style.transform = `rotate(${20}deg)`;
+        }
+        document.onmousemove = (event) => {
+          moveAt(event);
+        };
+        circle.onmouseup = () => {
+          document.onmousemove = null;
+          circle.onmouseup = null;
+        };
+      };
+    },
+    methods: {},
+  };
+</script>
 <style>
-  .circle{
+  .circle {
     background-color: red;
     width: 500px;
     height: 500px;
     border-radius: 50%;
   }
-  .srt{
+
+  .str {
     width: 12px;
     height: 100px;
     background-color: black;
